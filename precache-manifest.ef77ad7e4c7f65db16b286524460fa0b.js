08e99a0138c70294f686fdb5984ad02578aa970b
Gjc452 <1796444035@qq.com>
2021-02-20 22:44:40 +0800

    deploy

---
+++ precache-manifest.ef77ad7e4c7f65db16b286524460fa0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ac5601018e4f9cd168",
+    "url": "/Drib-money-website/css/app.3eb29c0b.css"
+  },
+  {
+    "revision": "04af7c0de0eea0d1426b6266bde25d51",
+    "url": "/Drib-money-website/index.html"
+  },
+  {
+    "revision": "c7ac5601018e4f9cd168",
+    "url": "/Drib-money-website/js/app.753fb6c6.js"
+  },
+  {
+    "revision": "f18cfcc12e5198afd63a",
+    "url": "/Drib-money-website/js/chunk-vendors.93805bc4.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Drib-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Drib-money-website/robots.txt"
+  }
+]);
